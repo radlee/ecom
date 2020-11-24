25376caa1d64479571901f728e1b5c63051b3a41
radlee <prof.lolito@gmail.com>
2020-11-24 15:31:09 +0200

    added redux code, and implemented userReducer and userActions to header and App components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -10,36 +11,32 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChange(async userAuth => {
 
       if(userAuth) {
+
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapShot(snapShot => {
-          this.setState({
-            currentUser: {
+
+          setCurrentUser({
+           
               id: snapShot.id,
               ...snapShot.data()
-            }
-          }, () => {
-            console.log(this.state);
           });
         });
       } else {
-        this.setState({ currentUser: userAuth })
+        setCurrentUser(userAuth)
       }
     });
   }
@@ -51,16 +48,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={ this.state.currentUser }/>
+        <Header />
         <br /><hr /><br />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/' component={ HomePage } />
+          <Route path='/shop' component={ ShopPage } />
+          <Route path='/signin' component={ SignInAndSignUpPage } />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default  connect(null, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,25 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-
-}
+    apiKey: "AIzaSyDYEiGoah5b1PjdKEiW_ZRfImS9k_wmraU",
+    authDomain: "tns-db.firebaseapp.com",
+    databaseURL: "https://tns-db.firebaseio.com",
+    projectId: "tns-db",
+    storageBucket: "tns-db.appspot.com",
+    messagingSenderId: "1039925161452",
+    appId: "1:1039925161452:web:eddca7b820aea3d4e4a4c9",
+    measurementId: "G-NMWCEG5DZB"
+  }
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
+
     if(!userAuth) return;
 
     const userRef = firestore.doc(`users/${ userAuth }`);
     const snapShot = await userRef.get();
 
     if(!snapShot.exists) {
+        
         const { displayName, email } = userAuth;
         const createdAt = new Date();
 
