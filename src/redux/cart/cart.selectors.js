5399d689a4b50968f61c0ac39b037e43e5327e4e
radlee <prof.lolito@gmail.com>
2020-11-26 18:13:48 +0200

    Added Checkout Page and the Styles. Used dispatch shorthand

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,12 +4,25 @@ const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
     [selectCart],
-    (cart) => cart.cartItems
+    cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
-    [selectCartItems],
+    [selectCartItems, selectCartHidden],
     cartItems => cartItems.reduce(
         (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+    )
+)
+
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0
     )
 )
